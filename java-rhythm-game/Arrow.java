ff164850d60968d3a98af9d4d5fac938b4f56e32
Kenny Lee <113561484+kennylwe@users.noreply.github.com>
2025-05-12 09:10:52 -0700

    Create Arrow.java

---
+++ java-rhythm-game/Arrow.java
@@ -0,0 +1,35 @@
+public class Arrow{
+  private String type;
+  public int speed;
+  public int x, y;
+  
+  public Arrow(String dir) {
+    type = dir;
+    speed = 5;
+    if (type.equals("RIGHT")) {
+      x = 750;
+    } else if (type.equals("LEFT")) {
+      x = 150;
+    } else if (type.equals("DOWN")) {
+      x = 350;
+    } else if (type.equals("UP")) {
+      x = 550;
+    }
+    
+  }
+  
+  public void update() {
+    y += speed;
+    
+    
+  }
+  public String getType() {
+    return type;
+  }
+  public int gety() {
+    return y;
+  }
+  
+  
+ 
+}
