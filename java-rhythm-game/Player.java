63adf5f913a6c6101cf4df1fc7e7edb27c128af7
Kenny Lee <113561484+kennylwe@users.noreply.github.com>
2025-05-14 09:38:17 -0700

    big of new stuff

--- java-rhythm-game/Player.java
+++ java-rhythm-game/Player.java
@@ -10,17 +10,56 @@ public class Player implements KeyListener{
   private boolean clickable;
   private boolean clicked;
   private int score;
+  private int rarrowposx;
+  private int darrowposx;
+  private int uparrowposx;
+  private int leftarrowposx;
+  private boolean[] holding = new boolean[4];
+  private int[] stuff = new int[4];
+  
   
   public Player() {
 
     score = 0;
+    rarrowposx = 750;
+    darrowposx = 350;
+    uparrowposx = 550;
+    leftarrowposx = 150;
+    
+    holding = new boolean[]{false, false, false, false};
+    stuff = new int[]{rarrowposx, darrowposx, uparrowposx, leftarrowposx};
+      
 
     
     
   }
+  public boolean[] getHeldArrows() {
+    return holding;
+  }
+  
+  
+  public int[] getAllPos() {
+    return stuff;
+  }
+  
   
   public void keyReleased (KeyEvent e) {
-    
+    if (e.getKeyCode() == KeyEvent.VK_UP) {
+      
+      holding[2] = false;
+    }
+    else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+      
+      holding[1] = false;
+    }
+    else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+      
+      holding[0] = false;
+    }
+    else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+      
+      holding[3] = false;
+    }
   }
   
   public void keyTyped (KeyEvent e) {
@@ -30,17 +69,19 @@ public class Player implements KeyListener{
   public void keyPressed(KeyEvent e) {
     if (e.getKeyCode() == KeyEvent.VK_UP) {
       arrow = "UP";
+      holding[2] = true;
     }
     else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
       arrow = "DOWN";
+      holding[1] = true;
     }
     else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
       arrow = "RIGHT";
-      System.out.println("right");
+      holding[0] = true;
     }
     else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
       arrow = "LEFT";
-      System.out.println("left");
+      holding[3] = true;
     }
   }
   
